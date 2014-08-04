4596ea7eafb87f1d31d5df2978cf194e886e74f2
David Smyth <david.smyth@millennium-space.com>
2014-08-04 11:30:43 -0700

    Fridaay end of day

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  # http://davidcel.is/blog/2012/09/06/stop-validating-email-addresses-with-regex/
+  # We will really validate using an email round-trip
+  validates :email, presence: true, 
+    format: { with: /@/ }, uniqueness: { case_sensitive: false }
+  before_save { self.email = email.downcase }
+  
+  has_secure_password
 end
