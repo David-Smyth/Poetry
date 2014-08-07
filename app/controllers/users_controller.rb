03eb0ff67cc9331eb677152b1e83dd18f7b9bd31
David Smyth <david.smyth@millennium-space.com>
2014-08-07 11:31:49 -0700

    Signup with status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    # For safety, never accept the parameters directly from the web page
+    # (really the POST request) as a fake page could push all sorts of
+    # evil parameters back. So we filter the parameters using the private
+    # method filtered_user_params that ensures the required parameter set
+    # and the permitted parameters within that set (hash)
+    @user = User.new(filter_user_params)
     if @user.save
-      # Handle a successful save
+      flash[:success] = "Welcome to Poetry"
+      # We don't have a create_path page -- instead we show the 
+      # profile for the user (e.g., the user page, which is a
+      # redirect to the user show page).
+      redirect_to @user
     else
       render 'new'
     end
@@ -18,7 +27,10 @@ class UsersController < ApplicationController
   end
 
   private
-    def user_params
+    # Filter parameters coming from user web page (really, POST) to ensure
+    # we only pay attention to the proper parameters. A malicious POST will
+    # not put bogus junk into the instance or database.
+    def filter_user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- spec/requests/user_signup_spec.rb
+++ spec/requests/user_signup_spec.rb
@@ -16,6 +16,12 @@ describe "User Signup Page" do
     it "should not create a user" do
       expect { click_button submit }.not_to change(User, :count)
     end
+
+    describe "after submission" do
+      before { click_button submit }
+      it { should have_title('Sign up')}
+      it { should have_content('error')}
+    end
   end
 
   describe "Fill in form with valid data" do
@@ -28,6 +34,14 @@ describe "User Signup Page" do
 
     it "should create a user" do
       expect { click_button submit }.to change(User, :count).by(1)
+    end
+
+    describe "after saving the user" do
+      before { click_button submit }
+      let(:indexed_email) { "Capt.David.Smyth@gmail.com".downcase }
+      let(:user) { User.find_by(lc_email: indexed_email) }
+      it { should have_title(user.name) }
+      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
     end
   end
 end
